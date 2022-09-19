ecca977801dd13ef22a240e2044f285630792522
Satya Prakash Pandey <43215884+satyapprakash98@users.noreply.github.com>
2022-09-20 01:16:00 +0530

    Add files via upload
    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Google.Apis.Admin.Directory.directory_v1.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MyWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        WebDBContext db = new WebDBContext();
+
+        public IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        public IActionResult Submit(TblUserDetail tblUserDetail)
+        {
+            db.TblUserDetails.Add(tblUserDetail);
+            db.SaveChanges();
+           return RedirectToAction("Index");
+        }
+
+        public IActionResult List()
+        {
+            var list = db.TblUserDetails;
+            return View(list);
+        }
+
+        
+    }
+}
---
+++ Models/WebDBContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace MyWebApplication.Models
+{
+    public partial class WebDBContext : DbContext
+    {
+        public WebDBContext()
+        {
+        }
+
+        public WebDBContext(DbContextOptions<WebDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TblUserDetail> TblUserDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=Image02\\SQLEXPRESS;Initial Catalog=WebDB;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<TblUserDetail>(entity =>
+            {
+                entity.ToTable("tblUserDetails");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Age)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
